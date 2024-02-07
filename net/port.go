623885c57c0e711ecdc016ce44e22b376c41edae
Jose Ayala <joseayala45@outlook.es>
2024-02-06 19:59:13 -0500

    Escaneador de puertos haciendo uso de la concurrencia.

--- net/port.go
+++ net/port.go
@@ -1,20 +1,31 @@
 package main
 
 import (
+	"flag"
 	"fmt"
 	"net"
+	"sync"
 )
 
+var site = flag.String("site", "scanme.nmap.org", "url to scan")
+
 func main() {
-	for i := 0; i < 100; i++ {
-		//revisar los puertos que estan en el rango  --> se espera realizar conexión a alguno de estos puertos
-		//Dial -> conectarse al protocolo mencionado
-		conn, err := net.Dial("tcp", fmt.Sprintf("%s:%d", "scanme.nmap.org", i))
-		if err != nil {
-			//Se crea un continue para que el programa continúe buscando para realizar una conexión
-			continue
-		}
-		conn.Close()
-		fmt.Printf("Port %d is open \n", i)
+	flag.Parse()
+	var wg sync.WaitGroup
+	for i := 0; i < 65535; i++ {
+		wg.Add(1)
+		go func(port int) {
+			defer wg.Done()
+			//revisar los puertos que estan en el rango  --> se espera realizar conexión a alguno de estos puertos
+			//Dial -> conectarse al protocolo mencionado
+			conn, err := net.Dial("tcp", fmt.Sprintf("%s:%d", *site, port))
+			if err != nil {
+				//Se crea un continue para que el programa continúe buscando para realizar una conexión
+				return
+			}
+			conn.Close()
+			fmt.Printf("Port %d is open \n", port)
+		}(i)
 	}
+	wg.Wait()
 }
